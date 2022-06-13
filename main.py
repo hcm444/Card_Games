554d621fe6b2915e212f9261354ef06cfa0ca7dd
Henry <32826270+hcm444@users.noreply.github.com>
2022-06-13 01:30:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ import random
 from collections import Counter
 import time
 
-#HENRY MEIER
-#This is a simplistic deck of cards generator. The objective here is to generate a Royal Flush
-#Program takes 5 to 45 seconds
-
-
 royal = False
 start_time = time.time()
+iteration = 1
 while royal == False:
     vals = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
     # values
@@ -20,7 +16,7 @@ while royal == False:
     random.shuffle(deck)  # shuffle
 
     poker_hand = deck[0:5]  # take first five random values for hand
-    #poker_hand = [('king', 'clubs'), ('ace', 'clubs'), ('queen', 'clubs'), ('jack', 'clubs'), ('10', 'clubs')]
+    # poker_hand = [('king', 'clubs'), ('ace', 'clubs'), ('queen', 'clubs'), ('jack', 'clubs'), ('10', 'clubs')]
     poker_hand_vals = []
     poker_hand_suits = []
     c = 0
@@ -30,25 +26,25 @@ while royal == False:
         poker_hand_vals.append(val)
         poker_hand_suits.append(suit)
         print('The %s of %s' % (val, suit))
-    #print(poker_hand)
+    # print(poker_hand)
     # print(poker_hand_vals,poker_hand_suits)
 
     suit_counter = Counter(poker_hand_suits)
     val_counter = Counter(poker_hand_vals)
-    #print()
+    # print()
     # dictionaries
-    #print(val_counter.most_common(13))
+    # print(val_counter.most_common(13))
     # count the occurrences of values
-    #print(suit_counter.most_common(4))
+    # print(suit_counter.most_common(4))
     # count the occurrences of suits
 
     x = []
     for i in suit_counter.most_common(1):
-        #the most common suit
+        # the most common suit
         y = str(i[1]) + " of a kind"
-        #the occurance ofthe most common suit
+        # the occurance ofthe most common suit
     for i in val_counter.most_common(5):
-        #for the five cards
+        # for the five cards
         x.append(i[0])
 
         # append every value to a new list
@@ -56,5 +52,5 @@ while royal == False:
         royal = True
         print("Royal Flush")
         print("--- %s seconds ---" % (time.time() - start_time))
-
-
+        print("--- %s hands ---" % iteration)
+    iteration += 1
