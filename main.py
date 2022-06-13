390fdb6e1fe17ec24a589800c447ed1d5e075072
Henry <32826270+hcm444@users.noreply.github.com>
2022-06-13 01:40:12 -0700

    Update main.py
    Added comments.

--- main.py
+++ main.py
@@ -1,54 +1,68 @@
 import itertools
+# for deck creation
 import random
+# random number gen
 from collections import Counter
+# used dictionaries (Counter)
 import time
+# for statistics
 
 royal = False
+# boolean
 start_time = time.time()
+# init time
 iteration = 1
-while royal == False:
+# count interations
+while not royal:
     vals = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
-    # values
+    # values for cards
     suits = ['spades', 'clubs', 'hearts', 'diamonds']
-    # four suits
+    # four suits of cards
     deck = list(itertools.product(vals, suits))
-
-    random.shuffle(deck)  # shuffle
-
+    # deck is not a hand this generates a deck with itertools
+    random.shuffle(deck)
+    # shuffle deck
     poker_hand = deck[0:5]  # take first five random values for hand
     # poker_hand = [('king', 'clubs'), ('ace', 'clubs'), ('queen', 'clubs'), ('jack', 'clubs'), ('10', 'clubs')]
+    # test victory hand above
     poker_hand_vals = []
+    # array for vals
     poker_hand_suits = []
-    c = 0
+    # array for suits
     print()
     print('Your hand:')
     for val, suit in poker_hand:
         poker_hand_vals.append(val)
+        # append to new arrays for card logic
         poker_hand_suits.append(suit)
+        # append to new arrays for card logic
         print('The %s of %s' % (val, suit))
+        # print method for 5 different cards in hand
     # print(poker_hand)
     # print(poker_hand_vals,poker_hand_suits)
 
     suit_counter = Counter(poker_hand_suits)
+    # create new Counter object for suits
     val_counter = Counter(poker_hand_vals)
+    # create new Counter object for vals
+
     # print()
     # dictionaries
     # print(val_counter.most_common(13))
     # count the occurrences of values
     # print(suit_counter.most_common(4))
     # count the occurrences of suits
-
-    x = []
+    new_val_list = []
+    # seperate the five future values of the hand
     for i in suit_counter.most_common(1):
         # the most common suit
         y = str(i[1]) + " of a kind"
-        # the occurance ofthe most common suit
+        # "5 of a kind"
     for i in val_counter.most_common(5):
         # for the five cards
-        x.append(i[0])
-
+        new_val_list.append(i[0])
         # append every value to a new list
-    if sorted(x) == ['10', 'ace', 'jack', 'king', 'queen'] and y == "5 of a kind":
+    if sorted(new_val_list) == ['10', 'ace', 'jack', 'king', 'queen'] and y == "5 of a kind":
         royal = True
         print("Royal Flush")
         print("--- %s seconds ---" % (time.time() - start_time))
